cb314bb8e5686b867db1470f0d8e7c3205bd5e4a
Rami Rahikkala <rami.rahikkala@vincit.fi>
2023-01-29 10:44:00 +0200

    Add timer. This will convert to monitoring process later.
    Signed-off-by: Rami Rahikkala <rami.rahikkala@vincit.fi>

--- ruuvibot/ruuvibot/ruuvibot.py
+++ ruuvibot/ruuvibot/ruuvibot.py
@@ -83,6 +83,57 @@ async def heating_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     conn.close()
 
+### Timer
+
+async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the alarm message."""
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f"Beep! {job.data} seconds are over!")
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    if context is not None and context.job_queue is not None:
+        current_jobs = context.job_queue.get_jobs_by_name(name)
+        if not current_jobs:
+            return False
+        for job in current_jobs:
+            job.schedule_removal()
+    return True
+
+
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds>")
+
+
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
+
+
+### Timer end
+
 def main():
     # Read settings file
     with open('settings.json') as json_file:
@@ -98,6 +149,9 @@ def main():
     application.add_handler(CommandHandler('l', temperature))
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('h', heating_data))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+
     
     application.run_polling()
 
